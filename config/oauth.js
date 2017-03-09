51297750fe88d3134511d9db96865020ddaeeb13
AliceMarshall <marshall.alice92@gmail.com>
2017-03-09 20:41:35 +0000

    Env edit

--- config/oauth.js
+++ config/oauth.js
@@ -1,3 +1,5 @@
+const { env } = require('./environment');
+
 module.exports = {
   github: {
     loginURL: 'https://github.com/login/oauth/authorize',
@@ -16,9 +18,10 @@ module.exports = {
     profileURL: '#',
     clientId: process.env.PROJECT2_FB_CLIENT_ID,
     clientSecret: process.env.PROJECT2_FB_CLIENT_SECRET,
+    redirect_URI: env === 'production' ? 'https://evening-crag-17922.herokuapp.com/oauth/facebook' : 'http://localhost:3000/oauth/facebook',
     scope: 'user:email',
     getLoginURL() {
-      return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=http://localhost:3000/oauth/facebook`;
+      return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=${this.redirect_URI}`;
     }
   }
 };
