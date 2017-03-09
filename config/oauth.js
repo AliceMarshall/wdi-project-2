7508d43f4e1803a31dd0df519491f3e80f610331
AliceMarshall <marshall.alice92@gmail.com>
2017-03-09 21:21:36 +0000

    poo

--- config/oauth.js
+++ config/oauth.js
@@ -18,7 +18,7 @@ module.exports = {
     profileURL: 'http://api.facebook.com/user',
     clientId: process.env.PROJECT2_FB_CLIENT_ID,
     clientSecret: process.env.PROJECT2_FB_CLIENT_SECRET,
-    redirectURI: env === 'production' ? 'https://evening-crag-17922.herokuapp.com/oauth/facebook' : 'http://localhost:3000/oauth/facebook',
+    redirectURI: env === 'production' ? 'https://evening-crag-17922.herokuapp.com/oauth/facebook/' : 'http://localhost:3000/oauth/facebook/',
     getLoginURL() {
       return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=${this.redirectURI}`;
     }
